cab482fbb6cb71a1e79f7ecca7cfd2c78c107fbb
jovan.radivojsa <jovan.radivojsa@gmail.com>
2024-12-17 11:53:43 +0100

    Don't have time for this now, will do Day 17 tomorrow, after the interview.

---
+++ Sources/Days2024/Day17.swift
@@ -0,0 +1,21 @@
+// https://adventofcode.com/2024/day/17
+import Foundation
+
+class DaySeventeen: Day {
+    let dayNumber: Int = 17
+    let year = 2024
+
+    // MARK:- Part One
+
+    func partOne(input: String) -> String {
+    
+        return ""
+    }
+
+    // MARK:- Part Two
+
+    func partTwo(input: String) -> String {
+    
+        return ""
+    }
+}
